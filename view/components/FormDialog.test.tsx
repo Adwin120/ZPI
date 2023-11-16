c9d4b5898ac10ef78eed3c2a6eafa40a3cd7de64
Adwin120 <adam.winnik120@gmail.com>
2023-11-16 10:26:38 +0100

    test form validations

--- view/components/FormDialog.test.tsx
+++ view/components/FormDialog.test.tsx
@@ -2,23 +2,26 @@
  * @jest-environment jsdom
  */
 import React, { ComponentProps } from "react";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { userEvent } from "@testing-library/user-event";
 
 import FormDialog from "./FormDialog";
 import { TextField } from "@mui/material";
+import { z } from "zod";
 
 const FormDialogWithTestProps: React.FC<Partial<ComponentProps<typeof FormDialog>>> = ({
     children,
     onClose,
     open,
-    submitEndpoint,
+    onSubmit,
     title,
+    schema,
 }) => (
     <FormDialog
         title={title ?? ""}
-        submitEndpoint={submitEndpoint ?? ""}
+        onSubmit={onSubmit ?? (() => {})}
+        schema={schema ?? z.object({})}
         open={open ?? true}
         onClose={onClose ?? (() => {})}
     >
@@ -70,7 +73,78 @@ describe("FormDialog - generic form in modal dialog", () => {
         });
     });
 
-    it.todo("should close dialog when submitted");
+    const exampleValidationMessage = "please provide a valid email"
+    const exampleSchema = z.object({
+        name: z.string(),
+        email: z.string().email(exampleValidationMessage),
+    });
+
+    it("should submit valid values", async () => {
+        const user = userEvent.setup();
+        const onSubmitCallback = jest.fn();
+        const { getByRole } = render(
+            <FormDialogWithTestProps onSubmit={onSubmitCallback} schema={exampleSchema}>
+                <TextField name="name" defaultValue="Jan" label="name" />
+                <TextField name="email" defaultValue="Jan.Xinski@gmail.com" label="email" />
+            </FormDialogWithTestProps>
+        );
+
+        await user.click(getByRole("button", { name: /zapisz/i }));
+
+        waitFor(() => {
+            expect(onSubmitCallback).toHaveBeenCalledWith({
+                name: "Jan",
+                email: "Jan.Xinski@gmail.com",
+            });
+        });
+    });
+
+    it("should not submit invalid values", async () => {
+        const user = userEvent.setup();
+        const onSubmitCallback = jest.fn();
+        const { getByRole } = render(
+            <FormDialogWithTestProps onSubmit={onSubmitCallback} schema={exampleSchema}>
+                <TextField name="name" defaultValue="Jan" label="name" />
+                <TextField name="email" defaultValue="Jan.Xinski@gmail.com" label="email" />
+            </FormDialogWithTestProps>
+        );
+
+        await user.click(getByRole("button", { name: /zapisz/i }));
+
+        waitFor(() => {
+            expect(onSubmitCallback).not.toHaveBeenCalled();
+        });
+    });
+
+    it("should show helper text with message from schema when invalid", async () => {
+        const user = userEvent.setup();
+        const onSubmitCallback = jest.fn();
+        const { getByText, getByRole } = render(
+            <FormDialogWithTestProps onSubmit={onSubmitCallback} schema={exampleSchema}>
+                <TextField name="name" defaultValue="Jan" label="name" />
+                <TextField name="email" defaultValue="Jan.Xinski" label="email" />
+            </FormDialogWithTestProps>
+        );
+
+        await user.click(getByRole("button", { name: /zapisz/i }));
+
+        waitFor(() => {
+            expect(getByText(exampleValidationMessage)).toBeVisible();
+        })
+    });
+
+    it("should close dialog when submitted", async () => {
+        const user = userEvent.setup();
+        const onCloseCallback = jest.fn();
+        const { getByRole } = render(<FormDialogWithTestProps onClose={onCloseCallback} />);
+        const submitButton = getByRole("button", { name: /zapisz/i });
+
+        await user.click(submitButton);
+
+        expect(onCloseCallback).toHaveBeenCalledTimes(1);
+    });
+
+    //TODO: move to mutation manager
     it.todo("should show success message when submit succeeded");
     it.todo("should show error message when submit failed");
 });
