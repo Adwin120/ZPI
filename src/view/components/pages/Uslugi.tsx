d36313d1ab01427b38330c980fd20092d400a6ef
Adwin120 <adam.winnik120@gmail.com>
2023-12-04 08:53:53 +0100

    fix umowy not being editable

--- src/view/components/pages/Uslugi.tsx
+++ src/view/components/pages/Uslugi.tsx
@@ -4,7 +4,7 @@ import FormButton from "../layout/FormButton";
 import { deleteFromEndpoint, patchEndpoint, postToEndpoint } from "../../backendAccess";
 import { Usluga, uslugaSchema } from "../../../common/uslugaSchema";
 import FormTextField from "../forms/FormTextField";
-import DataTable from "../DataTable";
+import DataTable, { EditableColumnHeader } from "../DataTable";
 import { GridActionsCellItem } from "@mui/x-data-grid";
 
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
@@ -35,8 +35,8 @@ const Uslugi: React.FC<Props> = () => {
                         return { IdUsluga, ...rest };
                     }}
                     schema={[
-                        { field: "Nazwa", flex: 1, minWidth: 200 },
-                        { field: "Opis", flex: 3, minWidth: 300 },
+                        { field: "Nazwa", flex: 1, minWidth: 200, editable: true, renderHeader: EditableColumnHeader },
+                        { field: "Opis", flex: 3, minWidth: 300, editable: true, renderHeader: EditableColumnHeader},
                         {
                             field: "opcje",
                             width: 50,
